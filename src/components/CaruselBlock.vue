9bef742f1e31902b92d0705dfb1bd3714f165be9
Yuliia Ishchenko <golovinadic@gmail.com>
2024-04-23 14:57:19 +0200

    q

--- src/components/CaruselBlock.vue
+++ src/components/CaruselBlock.vue
@@ -3,7 +3,7 @@
     <Carousel :value="products" :numVisible="3" :numScroll="1" circular :autoplayInterval="3000">
       <template #item="slotProps">
         <img
-          src="/src/assets/img/Aktuality-obrazek2.jpg"
+          :src="`/src/assets/img/Aktuality-obrazek2.jpg`"
           :alt="slotProps.data.name"
           class="w-full border-round img-carusel" />
       </template>
